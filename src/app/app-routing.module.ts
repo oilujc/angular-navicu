c60ff601fb737ab720f915876f48537c59d6ac39
Jorge <jrojas@jacidi.com>
2019-09-04 15:13:35 -0400

    urlsconfiguradas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  	{
+    	path: 'rol', redirectTo: 'login', pathMatch: 'full'
+  	},
+	{
+	    path: 'rol',
+	    loadChildren: () => import('./rol/rol.module').then(m => m.RolModule)
+  	},
+  	{ 
+    	path: '**',  redirectTo: 'rol', pathMatch: 'full' 
+  	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
